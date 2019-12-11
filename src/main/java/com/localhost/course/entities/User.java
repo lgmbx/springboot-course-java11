d3d677c423cd82c774dcacf361a4d93142064286
Gustavo <gustavo_x@live.com>
2019-12-11 14:51:45 -0300

    H2 database, test profile, JPA

--- src/main/java/com/localhost/course/entities/User.java
+++ src/main/java/com/localhost/course/entities/User.java
@@ -2,11 +2,19 @@ package com.localhost.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //
 public class User implements Serializable {
 	//serializable
 	private static final long serialVersionUID = 1L;
 	
 	//attributes
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
--- src/main/java/com/localhost/course/resources/UserResource.java
+++ src/main/java/com/localhost/course/resources/UserResource.java
@@ -11,7 +11,7 @@ import com.localhost.course.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	@GetMapping
+	@GetMapping	
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Maria", "maria@gmail.com","999999","1234");
 		return ResponseEntity.ok().body(u);
